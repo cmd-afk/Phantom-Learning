7cb78e80d5ddf9147a40eb02a061a650589f56ef
cmd-afk <brandonbyron624@icloud.com>
2026-03-14 14:52:58 -0400

    Add files via upload

---
+++ blocked-emails.js
@@ -0,0 +1,8 @@
+/**
+ * Emails to block from using Phantom Learning.
+ * Add one email per line (or keep as array entries).
+ * Comparison is case-insensitive.
+ */
+window.BLOCKED_EMAILS = [
+  "kirk.sarah@gmail.com"
+];
